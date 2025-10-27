bf0ff86f6d24c4af53d565313c8a2889d810d344
Emmanuel Ibuoye <ibuoyeemmanuel8@gmail.com>
2025-10-27 22:00:48 +0100

    New feature and ui improvements

--- tasky/script.js
+++ tasky/script.js
@@ -1,6 +1,11 @@
 const addTask = document.querySelector('#add-task') // Getting the form for adding new task
 const tasks = document.getElementById('tasks')
 
+const taskStatus = document.querySelector('.task-status')
+
+if (tasks.length == 0) {
+    taskStatus.textContent = 'No Tasks'
+}
 
 addTask.addEventListener('submit', (event) => {
     event.preventDefault() // prevents the page from reloading
@@ -9,8 +14,8 @@ addTask.addEventListener('submit', (event) => {
     let newTask = task.value
     let validTask = newTask.trim()
 
-    if (validTask.length != 0){
-        
+    if (validTask.length != 0) {
+
         const br = document.createElement('br')
         const check = document.createElement('input')
 
@@ -19,12 +24,12 @@ addTask.addEventListener('submit', (event) => {
         const label = document.createElement('label')
 
         label.textContent = validTask
-        tasks.appendChild(br)
         tasks.appendChild(check)
         tasks.appendChild(label)
+        tasks.appendChild(br)
 
 
-        task.value = ''
+        task.value = '' // resets the textarea
 
     } else {
         userReply = prompt('No task added. Do you want to continue?(y/n)').trim().toLowerCase()
@@ -36,22 +41,20 @@ addTask.addEventListener('submit', (event) => {
 
             check.type = 'checkbox'
             check.setAttribute('class', 'checkbox')
-            
+
             const label = document.createElement('label')
 
             label.textContent = 'No task'
             label.style.color = 'grey'
-            tasks.appendChild(br)
             tasks.appendChild(check)
             tasks.appendChild(label)
-
-        } 
-
+            tasks.appendChild(br)
+        }
     }
 
+    let numOfTask = tasks.length
 
-
-
+    if (tasks.length != 0) {
+        taskStatus.textContent = `You have ${numOfTask} tasks`
+    }
 })
-
-
